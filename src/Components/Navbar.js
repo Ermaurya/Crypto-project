c353bab439c9c0209fdc3d065632f6daebf5fd9f
Shubham maurya <49495079+Ermaurya@users.noreply.github.com>
2023-02-07 08:54:34 +0530

    Update Navbar.js

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,9 +1,31 @@
-import React from 'react'
-import { Menu, Typography, Avatar } from 'antd';
+import React ,{ useState,useEffect}from 'react'
+// import { Button, Menu, Typography, Avatar } from 'antd';
+import { Button, Menu, Typography, Avatar } from 'antd';
 import { Link } from 'react-router-dom';
 import logos from '../images/cryptocurrency.png'
-import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined} from '@ant-design/icons';
+import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined,MenuOutlined} from '@ant-design/icons';
 const Navbar = () => {
+  const [activeMenu, setActiveMenu] = useState(true);
+  const [screenSize, setScreenSize] = useState(undefined);
+
+  useEffect(() => {
+    const handleResize = () => setScreenSize(window.innerWidth);
+
+    window.addEventListener('resize', handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  useEffect(() => {
+    if (screenSize <= 800) {
+      setActiveMenu(false);
+    } else {
+      setActiveMenu(true);
+    }
+  }, [screenSize]);
+
   return (
     <div className='nav-container'>
         <div className='logo-container'>
@@ -11,8 +33,10 @@ const Navbar = () => {
        <Typography.Title  level={2} className="logo">
         <Link to="/">Cryptoverse</Link>
        </Typography.Title>
+       <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
 
         </div>
+        {activeMenu && (
         <Menu theme="dark">
         <Menu.Item icon={<HomeOutlined />}>
           <Link to="/">Home</Link>
@@ -27,6 +51,7 @@ const Navbar = () => {
           <Link to="/news">News</Link>
         </Menu.Item>
       </Menu>
+        )}
     </div>
   )
 }
